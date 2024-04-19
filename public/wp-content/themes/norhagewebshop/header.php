4a97d06f010ace8fe63a4ecb70c2d2f7919d71aa
bram de leeuw <bramdeleeuw@gmail.com>
2024-04-19 16:55:38 +0200

    consent v2

--- public/wp-content/themes/norhagewebshop/header.php
+++ public/wp-content/themes/norhagewebshop/header.php
@@ -23,8 +23,10 @@
 		function gtag(){dataLayer.push(arguments);}
 
 		gtag('consent', 'default', {
-			'ad_storage' : 'denied',
-			'analytics_storage' : 'granted'
+			'ad_storage' : 			'denied',
+			'ad_user_data': 		'denied',
+			'ad_personalization': 	'denied',
+			'analytics_storage': 	'granted'
 		});
 
 		gtag('js', new Date());
