423ea2f6728ad80e61ea998622729cb33b300702
bram de leeuw <bramdeleeuw@gmail.com>
2024-05-31 22:53:40 +0200

    move scripts to footer

--- public/wp-content/themes/norhagewebshop/functions.php
+++ public/wp-content/themes/norhagewebshop/functions.php
@@ -188,7 +188,7 @@ add_filter("wp_nav_menu_objects",'norhage_wp_nav_menu_objects_start_in',10,2);
  */
 function norhagewebshop_scripts() {
 	wp_enqueue_style( 'norhagewebshop-style', get_stylesheet_uri(), array(), _G_VERSION );
-	wp_enqueue_script('norhagewebshop-misc', get_stylesheet_directory_uri() . '/js/frontend.js', ['jquery', 'wc-settings'], _G_VERSION);
+	wp_enqueue_script('norhagewebshop-misc', get_stylesheet_directory_uri() . '/js/frontend.js', ['jquery', 'wc-settings'], _G_VERSION, ['in_footer' => true]);
 	/*$myThemeParams = [
 		'test1'	=> 'lalala'
 	];
