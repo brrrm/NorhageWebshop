4dd4b38d5f35f23e79df71fb40773d6f819968cd
bram de leeuw <bramdeleeuw@gmail.com>
2024-06-18 16:25:30 +0200

    breadcrumbs

--- public/wp-content/themes/norhagewebshop/woocommerce/archive-product.php
+++ public/wp-content/themes/norhagewebshop/woocommerce/archive-product.php
@@ -31,6 +31,11 @@ do_action( 'woocommerce_before_main_content' );
 ?>
 <div class="entry-content">
 	<header class="woocommerce-products-header">
+		<?php
+			if ( function_exists('yoast_breadcrumb') ) {
+				yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+			}
+		?>
 		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 			<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 		<?php endif; ?>
