f580e9fca8b4c6f22655fd2e600594fed7248e78
bram de leeuw <bramdeleeuw@gmail.com>
2024-08-06 10:09:28 +0200

    selected filters in sidebar

--- public/wp-content/themes/norhagewebshop/sidebar-before_shop_loop.php
+++ public/wp-content/themes/norhagewebshop/sidebar-before_shop_loop.php
@@ -15,6 +15,12 @@
 		<h2><?php _e('Filters', 'norhagewebshop'); ?></h2>
 		<button class="close-btn"><?php _e('Close', 'norhagewebshop'); ?></button>
 
+
+		<div class="bapf_sfilter bapf_sfa_mt_hide selected_filters">
+			<div class="bapf_head"><h3>Selected filters</h3></div>
+			<div class="bapf_body"><div class="berocket_aapf_widget_selected_area"></div></div>
+		</div>
+
 		<?php dynamic_sidebar( 'before_shop_loop' ); ?>
 
 	</div>
