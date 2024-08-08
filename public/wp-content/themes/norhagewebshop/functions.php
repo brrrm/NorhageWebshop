8af3178624121debc8db5b6f0917ccf9b690949e
bram de leeuw <bramdeleeuw@gmail.com>
2024-08-08 13:34:04 +0200

    variable products add-to-cart label

--- public/wp-content/themes/norhagewebshop/functions.php
+++ public/wp-content/themes/norhagewebshop/functions.php
@@ -374,6 +374,15 @@ function norhage_woocommerce_loop_add_to_cart_link($link, $product, $args){
 			isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
 			__('Customize', 'norhagewebshop')
 		);
+	}else if($product->is_type('variable')){
+		$link = sprintf(
+			'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+			esc_url( $product->add_to_cart_url() ),
+			esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+			esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+			isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+			__('Customize', 'norhagewebshop')
+		);
 	}
 	return $link;
 }
