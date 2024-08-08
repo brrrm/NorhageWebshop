99d1800636439ab04abcf52abeb8cd676c66cf71
bram de leeuw <bramdeleeuw@gmail.com>
2024-08-08 12:51:42 +0200

    fix add-to0cart button in catalog pages

--- public/wp-content/themes/norhagewebshop/functions.php
+++ public/wp-content/themes/norhagewebshop/functions.php
@@ -358,6 +358,27 @@ add_filter( 'post_thumbnail_id', function($thumbnail_id, $post ){
 
 
 /**
+ * Alter add-to-cart links in catalog for products with cutting fee
+ * */
+add_filter('woocommerce_loop_add_to_cart_link', 'norhage_woocommerce_loop_add_to_cart_link', 10, 3);
+function norhage_woocommerce_loop_add_to_cart_link($link, $product, $args){
+	$cutting_fee = get_field('cutting_fee', $product->ID);
+	$has_cutting_fee = ($cutting_fee && $cutting_fee > 0)? true : false;
+
+	if($product->is_type('simple') && $has_cutting_fee){
+		$link = sprintf(
+			'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+			esc_url( $product->get_permalink() ),
+			esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+			'button product_type_variable add_to_cart_button',
+			isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+			__('Customize', 'norhagewebshop')
+		);
+	}
+	return $link;
+}
+
+/**
  * Function for `woocommerce_add_cart_item_data` filter-hook.
  * add the addons for variable products to the cart_item_data.
  * 
