37b0a010bc6f19439763c054e06623063f934ab3
bram de leeuw <bramdeleeuw@gmail.com>
2024-08-13 12:10:29 +0200

    tweede test

--- public/wp-content/plugins/garbo-product-feed/feed.php
+++ public/wp-content/plugins/garbo-product-feed/feed.php
@@ -1,5 +1,15 @@
 <?php
 
 error_log('hhhihih');
-echo '<h1>lalala</h1>';
+$doc = new DomDocument('1.0'); 
+$xmlRoot = $doc->createElement("rss");
+$xmlRoot = $doc->appendChild($xmlRoot);
+$xmlRoot->setAttribute('xmlns:g', 'http://base.google.com/ns/1.0');
+$xmlRoot->setAttribute('version', '2.0');
+
+$channelNode = $xmlRoot->appendChild($doc->createElement('channel'));
+$channelNode->appendChild($doc->createElement('title', 'lalala'));
+$channelNode->appendChild($doc->createElement('link', 'https://nu.nl'));
+$channelNode->appendChild($doc->createElement('description', 'oohlalala'));
+echo $doc->saveXML();
 exit();
