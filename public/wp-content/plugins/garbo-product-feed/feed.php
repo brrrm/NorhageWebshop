a56abf8ab14968a1297e27c9ddecc5d1864b1529
bram de leeuw <bramdeleeuw@gmail.com>
2024-08-13 12:00:03 +0200

    custom plugin test

---
+++ public/wp-content/plugins/garbo-product-feed/feed.php
@@ -0,0 +1,5 @@
+<?php
+
+error_log('hhhihih');
+echo '<h1>lalala</h1>';
+exit();
---
+++ public/wp-content/plugins/garbo-product-feed/garbo-product-feed.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Plugin Name:  Garbo product feeds
+Plugin URI:   https://www.garbo.nl
+Description:  Garbo product feeds
+Version:      1.0
+Author:       Garbo
+Author URI:   https://www.garbo.nl
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  garbo_product_feeds
+Domain Path:  /languages
+*/
+
+add_action( 'init', 'wpse9870_init_external' );
+function wpse9870_init_external()
+{
+    error_log('cool init');
+    global $wp_rewrite;
+    $plugin_url = plugins_url( 'feed.php', __FILE__ );
+    $plugin_url = substr( $plugin_url, strlen( home_url() ) + 1 );
+    error_log($plugin_url);
+    // The pattern is prefixed with '^'
+    // The substitution is prefixed with the "home root", at least a '/'
+    // This is equivalent to appending it to `non_wp_rules`
+    $wp_rewrite->add_external_rule( '^feel/$', $plugin_url );
+}
