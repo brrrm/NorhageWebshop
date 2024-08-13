c28ab30f3db922a490a7fae74b0de012eb812c22
bram de leeuw <bramdeleeuw@gmail.com>
2024-08-13 13:25:23 +0200

    test 3

--- public/wp-content/plugins/garbo-product-feed/feed.php
+++ public/wp-content/plugins/garbo-product-feed/feed.php
@@ -1,6 +1,16 @@
 <?php
 
-error_log('hhhihih');
+require_once("wp-load.php");
+
+$args =  array(
+	'posts_per_page'   => -1,
+    'post_type'     => 'product',
+    'post_status'   => 'publish',
+    'lang'			=> 'nb'
+);
+$the_query = new WP_Query($args);
+$posts = $the_query->get_posts();
+
 $doc = new DomDocument('1.0'); 
 $xmlRoot = $doc->createElement("rss");
 $xmlRoot = $doc->appendChild($xmlRoot);
@@ -11,5 +21,29 @@ $channelNode = $xmlRoot->appendChild($doc->createElement('channel'));
 $channelNode->appendChild($doc->createElement('title', 'lalala'));
 $channelNode->appendChild($doc->createElement('link', 'https://nu.nl'));
 $channelNode->appendChild($doc->createElement('description', 'oohlalala'));
+
+foreach ($posts as $prekes){
+	$produktas = wc_get_product($prekes->ID);
+	if (!empty($produktas->name) && !empty($produktas->description) && !empty($produktas->description) && !empty($produktas->price)){
+	if(strlen($produktas->name) > 70){$productName = mb_substr($produktas->name, 0, 67,'UTF-8').('...');}else{$productName = $produktas->name;};	
+		
+	$itemNode = $channelNode->appendChild($doc->createElement('item'));
+	$itemNode->appendChild($doc->createElement('g:id', $prekes->ID));
+	$itemNode->appendChild($doc->createElement('g:title', $productName));
+	$itemNode->appendChild($doc->createElement('g:description',   str_replace("&nbsp;", '',strip_tags($produktas->description))));
+	$itemNode->appendChild($doc->createElement('g:link', get_permalink($prekes->ID)));
+	$itemNode->appendChild($doc->createElement('g:condition', 'new'));
+	$itemNode->appendChild($doc->createElement('g:image_link', get_the_post_thumbnail_url($prekes->ID)));
+	$itemNode->appendChild($doc->createElement('g:availability', 'in stock'));
+	$itemNode->appendChild($doc->createElement('g:price', number_format(str_replace(",", '',$produktas->price), 2).' NOK'));
+	if ($produktas->is_on_sale() && !empty($produktas->get_sale_price())){
+	$itemNode->appendChild($doc->createElement('g:sale_price',  number_format(str_replace(",", '',$produktas->get_sale_price()), 2).' NOK'));
+	}
+	$itemNode->appendChild($doc->createElement('g:brand', 'TEHI AS'));
+	$itemNode->appendChild($doc->createElement('g:mpn', 'TEHI-AS-'.$prekes->ID));
+	}
+	
+}
+
 echo $doc->saveXML();
 exit();
--- public/wp-content/plugins/garbo-product-feed/garbo-product-feed.php
+++ public/wp-content/plugins/garbo-product-feed/garbo-product-feed.php
@@ -16,13 +16,12 @@ Domain Path:  /languages
 add_action( 'init', 'wpse9870_init_external' );
 function wpse9870_init_external()
 {
-    error_log('cool init');
     global $wp_rewrite;
     $plugin_url = plugins_url( 'feed.php', __FILE__ );
     $plugin_url = substr( $plugin_url, strlen( home_url() ) + 1 );
-    error_log($plugin_url);
+
     // The pattern is prefixed with '^'
     // The substitution is prefixed with the "home root", at least a '/'
     // This is equivalent to appending it to `non_wp_rules`
-    $wp_rewrite->add_external_rule( '^feel/$', $plugin_url );
+    $wp_rewrite->add_external_rule( 'products-xml/%$', $plugin_url );
 }
